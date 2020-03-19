45e8c4b8492dd8c083996381deaf574181af6011
Calum Moir <CalumbiaM@Calums-MacBook-Pro-2.local>
2020-03-19 20:03:23 +0000

    Add half of service registry

---
+++ service-registry/server/service.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const ServiceRegistry = require('./lib/ServiceRegistry');
+
+const service = express();
+// const ServiceRegistry = require('./ServiceRegistry');
+
+module.exports = (config) => {
+  const log = config.log();
+  const serviceRegistry = new ServiceRegistry(log);
+  // Add a request logging middleware in development mode
+  if (service.get('env') === 'development') {
+    service.use((req, res, next) => {
+      log.debug(`${req.method}: ${req.url}`);
+      return next();
+    });
+  }
+
+  service.put('/register/:servicename/:serviceversion/:serviceport', (req, res, next) => {
+    return next('Not implemented');
+  })
+
+  service.delete('/register/:servicename/:serviceversion/:serviceport', (req, res, next) => {
+    return next('Not implemented');
+  })
+
+  service.get('/find/:servicename/:serviceversion', (req, res, next) => {
+    return next('Not implemented');
+  })
+
+  // eslint-disable-next-line no-unused-vars
+  service.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    // Log out the error to the console
+    log.error(error);
+    return res.json({
+      error: {
+        message: error.message,
+      },
+    });
+  });
+  return service;
+};
