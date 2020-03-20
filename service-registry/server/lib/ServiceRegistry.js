18d82287fa0e2b8ecfb9a4e7212bb7a7e324cfb4
Phil Moir <pgmoir@gmail.com>
2020-03-20 12:59:52 +0000

    Add files up to stage 3

--- service-registry/server/lib/ServiceRegistry.js
+++ service-registry/server/lib/ServiceRegistry.js
@@ -1,3 +1,5 @@
+const semver = require('semver');
+
 class ServiceRegistry {
   constructor(log) {
     this.log = log;
@@ -5,7 +7,16 @@ class ServiceRegistry {
     this.timeout = 30;
   }
 
+  get(name, version) {
+    this.cleanup();
+    const candidates = Object.values(this.services)
+      .filter(service => service.name === name && semver.satisfies(service.version, version));
+    return candidates[Math.floor(Math.random() * candidates.length)];
+  }
+
   register(name, version, ip, port) {
+    this.cleanup();
+
     const key = name + version + ip + port;
 
     if (!this.services[key]) {
@@ -30,6 +41,16 @@ class ServiceRegistry {
     delete this.services[key];
     this.log.debug(`Deleted service ${name}, version ${version} at ${ip}:${port}`);
     return key;
+  }
+
+  cleanup() {
+    const now = Math.floor(new Date() / 1000);
+    Object.keys(this.services).forEach((key) => {
+      if (this.services[key].timestamp + this.timeout < now) {
+        delete this.services[key];
+        this.log.debug(`Removed service ${key}`);
+      }
+    })
   }
 }
 
--- service-registry/server/service.js
+++ service-registry/server/service.js
@@ -31,8 +31,11 @@ module.exports = (config) => {
     return res.json({ result: serviceKey });
   });
 
-  service.get('/find/:servicename/:serviceversion', (req, res, next) => {
-    return next('Not implemented');
+  service.get('/find/:servicename/:serviceversion', (req, res) => {
+    const { servicename, serviceversion } = req.params;
+    const svc = serviceRegistry.get(servicename, serviceversion);
+    if (!svc) return res.status(404).json({ result: 'Service not found' });
+    return res.json(svc);
   });
 
   // eslint-disable-next-line no-unused-vars
